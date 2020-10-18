c40f102e2f851ce3c24507a2859de43e88589123
Danex2 <khadane.miller@gmail.com>
2020-10-18 18:08:06 -0400

    still trying to get environment variables to work

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,8 +1,5 @@
 #!/bin/env bash
 
-echo TOKEN=$(aws ssm get-parameter --name "TOKEN" --query "Parameter.Value") >> .bash_profile
-echo export TOKEN >> .bash_profile
-
 cd /home/ec2-user/app
 
 pm2 stop omen
