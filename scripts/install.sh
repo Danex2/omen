c3ee8b9b27275cb4628595256725d6f3eaf3cd29
Danex2 <khadane.miller@gmail.com>
2020-10-18 15:57:52 -0400

    add token environment variable

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,6 +4,7 @@ set -e
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+export TOKEN=$(aws ssm get-parameter --name "TOKEN" --query "Parameter.Value")
 yum install -y nodejs
 yum install -y yarn
 npm install -g pm2
