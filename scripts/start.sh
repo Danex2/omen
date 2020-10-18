6e2a7d985872e6fcc15c46f5c31d9169f96dd95b
Danex2 <khadane.miller@gmail.com>
2020-10-18 17:18:39 -0400

    move setting environment variables to start of deployment process

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,4 @@
 
 cd /home/ec2-user/app
 
-echo TOKEN=$(aws ssm get-parameter --name "TOKEN" --query "Parameter.Value") >> .bash_profile
-echo export TOKEN >> .bash_profile
-
 pm2 start index.js --name omen
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,8 @@
 #!/bin/env bash
 
+echo TOKEN=$(aws ssm get-parameter --name "TOKEN" --query "Parameter.Value") >> .bash_profile
+echo export TOKEN >> .bash_profile
+
 cd /home/ec2-user/app
 
 pm2 stop omen
