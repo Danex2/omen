30cca1230a6931257fdcbda00f15c9b6b2d31e8c
Danex2 <khadane.miller@gmail.com>
2020-10-18 16:11:58 -0400

    move set environment variable step to start script

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,6 @@ set -e
 
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
 curl --silent --location https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
-echo TOKEN=$(aws ssm get-parameter --name "TOKEN" --query "Parameter.Value") >> .bash_profile
-echo export TOKEN >> .bash_profile
 yum install -y nodejs
 yum install -y yarn
 npm install -g pm2
